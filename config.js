32ba59847e7624ca523d16182eea9cbbfb0842a1
Michael Le-Reiver <m.lereiver@gmail.com>
2018-08-20 13:51:21 -0700

    created server and config files to establish initial connection

---
+++ config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('dotenv').config();
+
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/karpul',
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/karpul-test',
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
+};
